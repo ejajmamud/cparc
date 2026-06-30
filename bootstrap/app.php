790c1a213269a1b982c5943de2eb08c4a0c42f48
Md Ejaj Mahmud (MY) <ejaj.mahmud@shinjiru.com.my>
2026-07-01 02:30:06 +0800

    Security: clean up all storage diagnostics and temporary upload-file routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,9 +16,6 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             \App\Http\Middleware\SetLocale::class,
         ]);
-        $middleware->validateCsrfTokens(except: [
-            'upload-file',
-        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
