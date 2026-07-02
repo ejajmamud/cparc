15cb139d3c173af5097eb27241a4c9acfdf8cd9f
Md Ejaj Mahmud (MY) <ejaj.mahmud@shinjiru.com.my>
2026-07-02 21:20:10 +0800

    Fix 500: update Tab namespace to Filament v5 (Filament\Schemas\Components\Tabs\Tab)

--- app/Filament/Resources/AccountTransactionResource/Pages/ListAccountTransactions.php
+++ app/Filament/Resources/AccountTransactionResource/Pages/ListAccountTransactions.php
@@ -5,7 +5,7 @@ namespace App\Filament\Resources\AccountTransactionResource\Pages;
 use App\Filament\Resources\AccountTransactionResource;
 use App\Models\AccountTransaction;
 use Filament\Actions;
-use Filament\Resources\Components\Tab;
+use Filament\Schemas\Components\Tabs\Tab;
 use Filament\Resources\Pages\ListRecords;
 use Illuminate\Database\Eloquent\Builder;
 
